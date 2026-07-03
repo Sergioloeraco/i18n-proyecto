f700170f02513fb331c980c01824b2cd9430e2a0
Sergioloeraco <sergio.loeraco@gmail.com>
2026-07-03 11:52:07 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'i18n-proyecto',
+  webDir: 'www'
+};
+
+export default config;
